2da51804eb517ced2af86e4b63e552fc5138d785
bcseputetto <bcseputetto@gmail.com>
2024-01-12 09:40:54 +0800

    Create unifi_docker_upgrade.sh

---
+++ packages/generic/unifi_docker_upgrade.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+DIRECTORY_PATH=$1
+docker pull lscr.io/linuxserver/unifi-network-application:latest
+docker stop unifi-network-application
+sleep 10
+docker rm unifi-network-application
+sleep 10
+docker run -d \
+  --name=unifi-network-application \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ="Asia/Shanghai" \
+  -e MONGO_USER=unifi \
+  -e MONGO_PASS=unifi_istoreos \
+  -e MONGO_HOST=unifi-db \
+  -e MONGO_PORT=27017 \
+  -e MONGO_DBNAME=unifi \
+  -e MEM_LIMIT=1024 `#optional` \
+  -e MEM_STARTUP=1024 `#optional` \
+  -e MONGO_TLS= `#optional` \
+  -e MONGO_AUTHSOURCE= `#optional` \
+  -p 8443:8443 \
+  -p 3478:3478/udp \
+  -p 10001:10001/udp \
+  -p 8080:8080 \
+  -p 8843:8843 `#optional` \
+  -p 8880:8880 `#optional` \
+  -p 6789:6789 `#optional` \
+  -p 5514:5514/udp `#optional` \
+  -v $DIRECTORY_PATH/config:/config \
+  --link unifi-db \
+  --restart unless-stopped \
+  lscr.io/linuxserver/unifi-network-application:latest
